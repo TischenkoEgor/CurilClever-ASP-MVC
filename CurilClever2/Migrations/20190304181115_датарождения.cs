e12aa8c9d1f77db54d7c30a388fbd7a08d6361ee
TischenkoEgor <TischenkoEgor@gmail.com>
2019-03-04 22:56:34 +0300

    Обновление модели клиента
    + замена возраста на дату рождения
    + замена пола на перечисление
    + аннотации к полям модели
    + метод создания полного имени
    + метод вывода строки пола
    + миграции

---
+++ CurilClever2/Migrations/20190304181115_датарождения.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CurilClever2.Migrations
+{
+    public partial class датарождения : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Clients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondName",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PassportData",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirthday",
+                table: "Clients",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirthday",
+                table: "Clients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondName",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PassportData",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Clients",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- CurilClever2/Models/Client.cs
+++ CurilClever2/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,62 @@ namespace CurilClever2.Models
   public class Client
   {
     public int id { get; set; }
+
+    [Required(ErrorMessage = "Укажите имя")]
+    [MinLength(1, ErrorMessage = "не может быть короче 1 символа")]
+    [Display(Name = "Имя")]
     public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Укажите фамилию")]
+    [MinLength(1, ErrorMessage = "не может быть короче 3 символа")]
+    [Display(Name = "Фамилия")]
     public string SecondName { get; set; }
-    public int Age { get; set; }
-    public int Sex { get; set; }
+
+    [Required(ErrorMessage = "Укажите дату рождения")]
+    [DataType(DataType.Date)]
+    [Display(Name = "Дата рождения")]
+    public DateTime DateOfBirthday { get; set; }
+
+    [Required(ErrorMessage = "Укажите пол")]
+    [Display(Name = "Пол")]
+    public Gender Gender { get; set; }
+
+    [Required(ErrorMessage = "Укажите телефон")]
+    [Phone]
+    [Display(Name = "Телефон")]
     public string Phone { get; set; }
+
+    [Required(ErrorMessage = "Укажите адрес емейл")]
+    [EmailAddress]
+    [Display(Name = "Электронная почта")]
     public string Email { get; set; }
+
+    [Required(ErrorMessage = "Укажите пасспорт")]
+    [MinLength(3, ErrorMessage = "имя отеля не может быть короче 3 символов")]
+    [Display(Name = "Паспорт (серия, номер кем и когда выдан)")]
     public string PassportData { get; set; }
+    public string GetFullName()
+    {
+      return this.FirstName + " " + this.SecondName;
+    }
+    public int GetYearsFromBirth()
+    {
+      return (new DateTime(1, 1, 1) + (DateTime.Now - this.DateOfBirthday)).Year - 1;
+    }
+    public string GetGenderName()
+    {
+      if (Gender == Gender.Female)
+        return "жен.";
+      else
+        return "муж.";
+    }
+  }
+
+  public enum Gender
+  {
+    [Display(Name = "Муж.")]
+    Male,
+    [Display(Name = "Жен.")]
+    Female
   }
 }
