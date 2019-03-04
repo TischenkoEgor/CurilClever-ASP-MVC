4618084ba397f98470a92e14c9292d7201ee02bc
TischenkoEgor <TischenkoEgor@gmail.com>
2019-03-04 20:50:30 +0300

    Update Hotel.cs

--- CurilClever2/Models/Hotel.cs
+++ CurilClever2/Models/Hotel.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CurilClever2.Models
 {
   public class Hotel
   {
+
     public int id { get; set; }
+    [Required(ErrorMessage = "Не указано название отеля")]
+    [MinLength(3, ErrorMessage = "имя отеля не может быть короче 3 символов")]
+    [Display(Name = "Название отеля")]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "Укажите адрес отеля")]
+    [MinLength(9, ErrorMessage = "Адрес отеля не может быть короче 9 символов")]
+    [Display(Name = "Адрес отеля")]
     public string Addres { get; set; }
-    public int StarsRate { get; set; }
-    public int LocationType { get; set; }
-    public int Price { get; set; }
+    
+    [Required(ErrorMessage = "Укажите количество звезд отеля")]
+    [Range(1, 5, ErrorMessage = "Количество звезд строго от 1 до 5")]
+    [Display(Name = "Количество звезд отеля")]
+    public int? StarsRate { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "Цена не может быть меньше нуля")]
+    [Required(ErrorMessage = "Укажите цену в отеле")]
+    [Display(Name = "Стоимость ночи")]
+    public int? Price { get; set; }
   }
 }
