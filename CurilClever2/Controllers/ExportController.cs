66093c1aa11580bb3a5ca2a540e4f99cc996c063
TischenkoEgor <TischenkoEgor@gmail.com>
2019-05-08 17:56:34 +0300

    экспорт в эксель списка клиентов

---
+++ CurilClever2/Controllers/ExportController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CurilClever2.Helpers;
+using CurilClever2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurilClever2.Controllers
+{
+  public class ExportController : Controller
+  {
+    private CleverDBContext db;
+    public ExportController(CleverDBContext _db)
+    {
+      db = _db;
+    }
+    public FileResult Clients()
+    {
+      Response.Clear();
+      Byte[] exportdata = XLSExport.BuildClientsXLS(db.Clients.ToList());
+      return File(exportdata, "application/xlsx", "clients.xlsx");
+    }
+  }
+}
---
+++ CurilClever2/Helpers/XLSExport.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OfficeOpenXml;
+using System.Threading.Tasks;
+using System.IO;
+using CurilClever2.Models;
+using OfficeOpenXml.Style;
+
+namespace CurilClever2.Helpers
+{
+  public class XLSExport
+  {
+    public static Byte[] BuildClientsXLS(List<Client> clients)
+    {
+      MemoryStream ms = new MemoryStream();
+
+      ExcelPackage ExcelPkg = null;
+      ExcelPkg = new ExcelPackage(ms);
+
+      ExcelWorksheet wsSheet1 = ExcelPkg.Workbook.Worksheets.Add("клиенты");
+      AddHeader(wsSheet1);
+      
+      int position = 1;
+      foreach (Client client in clients)
+      {
+
+        try
+        {
+          AddClient(wsSheet1, client, position);
+          position++;
+        }
+        catch
+        {
+        }
+
+      }
+      wsSheet1.Protection.IsProtected = false;
+      wsSheet1.Protection.AllowSelectLockedCells = false;
+      //ExcelPkg.SaveAs(ms);
+      //ms.Seek((long)0, SeekOrigin.Begin);
+      return ExcelPkg.GetAsByteArray();
+    }
+    private static void AddClient(ExcelWorksheet wsSheet, Client client, int position)
+    {
+      int shift = 0;
+      wsSheet.Cells[2 + position, 2].Value = client.id.ToString();
+      wsSheet.Cells[2 + position, 3].Value = client.FIO.ToString();
+      wsSheet.Cells[2 + position, 4].Value = client.Gender.ToString();
+      wsSheet.Cells[2 + position, 5].Value = client.DateOfBirthday.ToLongDateString();
+      wsSheet.Cells[2 + position, 6].Value = client.PassportData.ToString();
+      wsSheet.Cells[2 + position, 7].Value = client.Phone.ToString();
+      wsSheet.Cells[2 + position, 8].Value = client.Email.ToString();
+    }
+    private static void AddHeader(ExcelWorksheet wsSheet)
+    {
+      wsSheet.Cells["A1:H1"].Merge = true;
+      wsSheet.Cells["A1:H1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+      
+      wsSheet.Column(1).Width = 5;
+      wsSheet.Cells[1, 1].Value = "сводная таблица клиентов по состоянию на " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString();
+
+      wsSheet.Column(2).Width = 12;
+      wsSheet.Cells[2, 2].Value = "ID клиента";
+
+      wsSheet.Column(3).Width = 30;
+      wsSheet.Cells[2, 3].Value = "ФИО клиента";
+
+      wsSheet.Column(4).Width = 16;
+      wsSheet.Cells[2, 4].Value = "ПОЛ клиента";
+
+      wsSheet.Column(5).Width = 24;
+      wsSheet.Cells[2, 5].Value = "Дата рождения клиента";
+
+      wsSheet.Column(6).Width = 45;
+      wsSheet.Cells[2, 6].Value = "Паспортные данные клиента";
+
+      wsSheet.Column(7).Width = 28;
+      wsSheet.Cells[2, 7].Value = "контактный телефон клиента";
+
+      wsSheet.Column(8).Width = 26;
+      wsSheet.Cells[2, 8].Value = "контактный емейл клиента";
+
+    }
+  }
+}
