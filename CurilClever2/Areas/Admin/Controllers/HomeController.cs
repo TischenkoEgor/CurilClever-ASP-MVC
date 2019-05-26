c82856acdfea5ebf280d92e7666bd2c4e99df881
TischenkoEgor <45077472+TischenkoEgor@users.noreply.github.com>
2019-05-26 21:43:31 +0300

    Вывод статистики в админке
    + новые ссылки для перехода по админке на страницу статистики

--- CurilClever2/Areas/Admin/Controllers/HomeController.cs
+++ CurilClever2/Areas/Admin/Controllers/HomeController.cs
@@ -4,16 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using CurilClever2.ViewModels;
+using CurilClever2.Models;
 
 namespace CurilClever2.Areas.Admin.Controllers
 {
   [Area("Admin")]
   [Authorize(Roles = "Admin")]
   public class HomeController : Controller
+  {
+    CleverDBContext db;
+    public HomeController(CleverDBContext _db)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+      db = _db;
     }
+    public IActionResult Index()
+    {
+      AdminHomePageViewModel adminHomePageViewModel = new AdminHomePageViewModel();
+      adminHomePageViewModel.BuildStat(db);
+      return View(adminHomePageViewModel);
+    }
+  }
 }
---
+++ CurilClever2/ViewModels/AdminHomePageViewModel.cs
@@ -0,0 +1,84 @@
+﻿using CurilClever2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurilClever2.ViewModels
+{
+  public class AdminHomePageViewModel
+  {
+    [Display(Name ="посещений главной страницы")]
+    public int CountHomePageVisits { get; set; }
+    [Display(Name = "посещений страницы управления клиентами")]
+    public int CountManageClientsPageVisits { get; set; }
+    [Display(Name = "посещений страницы управления отелями")]
+    public int CountManageHotelpageVisits { get; set; }
+    [Display(Name = "посещений страницы управления заявками")]
+    public int CountManageOrderPageVisits { get; set; }
+    public Dictionary<Hotel, int> HotelVisits { get; set; }
+    public Dictionary<Client, int> ClientVisits { get; set; }
+    public Dictionary<Order, int> OrderVisits { get; set; }
+
+    public AdminHomePageViewModel()
+    {
+      HotelVisits = new Dictionary<Hotel, int>();
+      ClientVisits = new Dictionary<Client, int>();
+      OrderVisits = new Dictionary<Order, int>();
+    }
+
+    public void BuildStat(CleverDBContext db)
+    {
+      // считаем посещения домашней страницы по количеству записей в базе, где путь состоит из строки "/"
+      CountHomePageVisits = db.Visits.Count(v => v.path.ToLower() == "/");
+      CountManageClientsPageVisits = db.Visits.Count(v => v.path.ToLower() == "/client");
+      CountManageHotelpageVisits = db.Visits.Count(v => v.path.ToLower() == "/hotel");
+      CountManageOrderPageVisits = db.Visits.Count(hui => hui.path.ToLower() == "/order");
+
+      foreach(Visit vizit in db.Visits.Where(v => v.path.ToLower().Contains("/client/details/")))
+      {
+        // получаем id отрезая часть ссылки до цифр
+        int id = int.Parse(vizit.path.ToLower().Replace("/client/details/", ""));
+        // получаем клиента из базы
+        Client client = db.Clients.Find(id);
+        // если клиент в базе не обнаружен: пропускам этот визит
+        if (client == null) continue;
+        // если такого клиента нет в словаре посещений, добавляем клиента в словарь
+        if (!ClientVisits.ContainsKey(client))
+          ClientVisits.Add(client, 0);
+        // увеличиваем количество посещений для этого клиента 
+        ClientVisits[client]++;
+      }
+      foreach (Visit vizit in db.Visits.Where(v => v.path.ToLower().Contains("/hotel/details/")))
+      {
+        // получаем id отрезая часть ссылки до цифр
+        int id = int.Parse(vizit.path.ToLower().Replace("/hotel/details/", ""));
+        // получаем отель из базы
+        Hotel hotel = db.Hotels.Find(id);
+        // если отель в базе не обнаружен: пропускам этот визит
+        if (hotel == null) continue;
+        // если такого отеля нет в словаре посещений, добавляем клиента в словарь
+        if (!HotelVisits.ContainsKey(hotel))
+          HotelVisits.Add(hotel, 0);
+        // увеличиваем количество посещений для этого отеля 
+        HotelVisits[hotel]++;
+      }
+      foreach (Visit vizit in db.Visits.Where(v => v.path.ToLower().Contains("/order/details/")))
+      {
+        // получаем id отрезая часть ссылки до цифр
+        int id = int.Parse(vizit.path.ToLower().Replace("/order/details/", ""));
+        // получаем заявку из базы
+        Order order = db.Orders.Find(id);
+        // если заявки в базе не обнаружен: пропускам этот визит
+        if (order == null) continue;
+        // если такого клиента нет в словаре посещений, добавляем клиента в словарь
+        if (!OrderVisits.ContainsKey(order))
+          OrderVisits.Add(order, 0);
+        // увеличиваем количество посещений для этого клиента 
+        OrderVisits[order]++;
+      }
+
+    }
+  }
+}
