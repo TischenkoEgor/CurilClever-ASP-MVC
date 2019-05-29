1762c44599daac82679f0c295eaba20d65511075
TischenkoEgor <45077472+TischenkoEgor@users.noreply.github.com>
2019-05-29 22:19:46 +0300

    снежинки по датам и обфусцированный JS код вида для печати

---
+++ CurilClever2/wwwroot/js/printview_obfus.js
@@ -0,0 +1,18 @@
+﻿// код обфусцирован онлайн на сайте https://www.obfuscator.io
+var a = ['cookie', 'print_view', '.jumbotron', 'css', 'display', 'none', '#print_view_btn', '#default_view_btn', 'null', 'block', '.container', 'max-width'];
+var b = function (c, d) { c = c - 0x0; var e = a[c]; return e; };
+function c() {
+  $[b('0x0')](b('0x1'), '1');
+  $('header')['css']('display', 'none');
+  $(b('0x2'))[b('0x3')](b('0x4'), b('0x5'));
+  $(b('0x6'))['css'](b('0x4'), b('0x5'));
+  $(b('0x7'))[b('0x3')](b('0x4'), 'block');
+}
+function d() {
+  $['cookie'](b('0x1'), b('0x8'));
+  $('header')[b('0x3')](b('0x4'), 'block');
+  $(b('0x2'))[b('0x3')](b('0x4'), b('0x9'));
+  $(b('0xa'))[b('0x3')](b('0xb'), '');
+  $(b('0x6'))[b('0x3')](b('0x4'), b('0x9'));
+  $(b('0x7'))['css']('display', b('0x5'));
+}
