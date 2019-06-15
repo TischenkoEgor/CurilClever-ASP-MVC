0cfbffd9ea7ee8108f418f00b51a30617ff99403
TischenkoEgor <45077472+TischenkoEgor@users.noreply.github.com>
2019-06-15 23:02:29 +0300

    Revert "перевод модели гендера"
    This reverts commit 5402d49aeccaf23456576fb67a28b85eb245d127.

--- CurilClever2/Models/Client.cs
+++ CurilClever2/Models/Client.cs
@@ -11,44 +11,44 @@ namespace CurilClever2.Models
   {
     public int id { get; set; }
 
-    [Required(ErrorMessage = "FirstNameRequired")]
-    [MinLength(1, ErrorMessage = "FirstNameMinLength")]
-    [Display(Name = "FirstName")]
-    [RegularExpression(@"^[a-zA-Zа-яА-Я]{1,}$", ErrorMessage = "FirstNameWrongName")]
+    [Required(ErrorMessage = "Укажите имя")]
+    [MinLength(1, ErrorMessage = "не может быть короче 1 символа")]
+    [Display(Name = "Имя")]
+    [RegularExpression(@"^[a-zA-Zа-яА-Я]{1,}$", ErrorMessage = "Неправильное имя")]
     public string FirstName { get; set; }
 
-    [Required(ErrorMessage = "SecondNameRequred")]
-    [MinLength(2, ErrorMessage = "SecondNameMinLength")]
-    [Display(Name = "SecondName")]
-    [RegularExpression(@"^[a-zA-Zа-яА-Я]{2,}$", ErrorMessage = "SecondNameWrongName")]
+    [Required(ErrorMessage = "Укажите фамилию")]
+    [MinLength(2, ErrorMessage = "не может быть короче чем 2 символа")]
+    [Display(Name = "Фамилия")]
+    [RegularExpression(@"^[a-zA-Zа-яА-Я]{2,}$", ErrorMessage = "Неправильная фамилия")]
     public string SecondName { get; set; }
 
     [NotMapped]
     public string FIO { get { return GetFullName();} }
 
-    [Required(ErrorMessage = "DateOfBirthdayReqired")]
+    [Required(ErrorMessage = "Укажите дату рождения")]
     [DataType(DataType.Date)]
-    [Display(Name = "DateOfBirthday")]
+    [Display(Name = "Дата рождения")]
     public DateTime DateOfBirthday { get; set; }
 
-    [Required(ErrorMessage = "GenderRequired")]
-    [Display(Name = "Gender")]
+    [Required(ErrorMessage = "Укажите пол")]
+    [Display(Name = "Пол")]
     public Gender Gender { get; set; }
 
-    [Required(ErrorMessage = "PhoneRequired")]
-    [DataType(DataType.PhoneNumber, ErrorMessage = "PhoneWrong")]
-    [RegularExpression(@"^((8|\+7)[\- ]?)?(\(?\d{3,4}\)?[\- ]?)?[\d\- ]{5,10}$", ErrorMessage = "PhoneInvalid")]
-    [Display(Name = "PhoneТелефон")]
+    [Required(ErrorMessage = "Укажите телефон")]
+    [DataType(DataType.PhoneNumber, ErrorMessage ="Это не номер телефона")]
+    [RegularExpression(@"^((8|\+7)[\- ]?)?(\(?\d{3,4}\)?[\- ]?)?[\d\- ]{5,10}$", ErrorMessage = "Not a valid phone number")]
+    [Display(Name = "Телефон")]
     public string Phone { get; set; }
 
-    [Required(ErrorMessage = "EmailRequired")]
+    [Required(ErrorMessage = "Укажите адрес емейл")]
     [EmailAddress]
-    [Display(Name = "Email")]
+    [Display(Name = "Электронная почта")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "PassportDataRequired")]
-    [MinLength(8, ErrorMessage = "PassportDataMinLength")]
-    [Display(Name = "PassportData")]
+    [Required(ErrorMessage = "Укажите пасспорт")]
+    [MinLength(3, ErrorMessage = "имя отеля не может быть короче 3 символов")]
+    [Display(Name = "Паспорт (серия, номер кем и когда выдан)")]
     public string PassportData { get; set; }
     public string GetFullName()
     {
@@ -79,9 +79,9 @@ namespace CurilClever2.Models
 
   public enum Gender
   {
-    [Display(Name = "Male")]
+    [Display(Name = "Муж.")]
     Male,
-    [Display(Name = "Female")]
+    [Display(Name = "Жен.")]
     Female
   }
 }
