c9e02f337664a1601ab22eb2809de779d766c84a
TischenkoEgor <45077472+TischenkoEgor@users.noreply.github.com>
2019-06-18 15:23:46 +0300

    локализация модели новости

--- CurilClever2/Models/News.cs
+++ CurilClever2/Models/News.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,21 +10,26 @@ namespace CurilClever2.Models
   {
     // первичный ключ в базе
     public int id { get; set; }
-   
+
     // Краткое описание
+    [Display(Name = "TextShort")]
     public string TextShort { get; set; }
-   
+
     // Подробное описание
+    [Display(Name = "TextFull")]
     public string TextFull { get; set; }
-  
+
     //  текст ссылки на объект новости, если есть
+    [Display(Name = "ObjectUrl")]
     public string ObjectUrl { get; set; }
 
     // Автор новости
     public int Userid { get; set; }
+    [Display(Name = "User")]
     public User User { get; set; }
 
     // Время создания новости
+    [Display(Name = "Created")]
     public DateTime Created { get; set; }
 
     // конструктор по умолчанию
