e385a65e5d972f93a1960f63a2231f00b6eb512d
TischenkoEgor <45077472+TischenkoEgor@users.noreply.github.com>
2019-06-18 14:06:45 +0300

    локализованная модель заявки

--- CurilClever2/Models/Order.cs
+++ CurilClever2/Models/Order.cs
@@ -10,55 +10,55 @@ namespace CurilClever2.Models
   {
     public int id { get; set; }
 
-    [Display(Name = "Заявка зарегистрирована")]
+    [Display(Name = "CreationDate")]
     public DateTime CreationDate { get; set; }
 
     [Required]
     [DataType(DataType.Date)]
-    [Display(Name = "Дата начала путешествия")]
+    [Display(Name = "BeginTravelDate")]
     public DateTime BeginTravelDate { get; set; }
 
     [Required]
     [DataType(DataType.Date)]
-    [Display(Name = "Дата конца путешествия")]
+    [Display(Name = "EndTravelDate")]
     public DateTime EndTravelDate { get; set; }
 
     [Required]
-    [Display(Name = "Отель")]
+    [Display(Name = "Hotel")]
     public int Hotelid { get; set; }
-    [Display(Name = "Отель")]
+    [Display(Name = "Hotel")]
     public Hotel Hotel { get; set; }
 
     [Required]
-    [Display(Name = "Клиент")]
+    [Display(Name = "Client")]
     public int Clientid { get; set; }
-    [Display(Name = "Клиент")]
+    [Display(Name = "Client")]
     public Client Client { get; set; }
 
     [Required]
-    [Display(Name = "Стоимость тура")]
+    [Display(Name = "Price")]
     [Range(0, int.MaxValue)]
     public int Price { get; set; }
 
-    [Display(Name = "Оплачено")]
+    [Display(Name = "TotalPaid")]
     [Range(0, int.MaxValue)]
     public int TotalPaid { get; set; }
 
     public IEnumerable<OrderComment> Comments { get; set; }
 
     [Required]
-    [Display(Name = "Статус оплаты")]
+    [Display(Name = "PayStatus")]
     public PayStatus PayStatus { get; set; }
   }
   public enum PayStatus
   {
-    [Display(Name = "не оплачено")]
+    [Display(Name = "Unpaid")]
     Unpaid,
-    [Display(Name = "внесена предоплата")]
+    [Display(Name = "PrePayed")]
     PrePayed,
-    [Display(Name = "оплачено полностью")]
+    [Display(Name = "Paid")]
     Paid,
-    [Display(Name = "иное")]
+    [Display(Name = "Custom")]
     Custom
   }
 }
