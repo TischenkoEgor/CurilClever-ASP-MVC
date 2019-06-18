81ce6aebd3b48b35ae684961a563191feb8903c0
TischenkoEgor <45077472+TischenkoEgor@users.noreply.github.com>
2019-06-18 15:56:06 +0300

    локализация управления своим аккаунтом
    + валидация имени пользователя

--- CurilClever2/Models/User.cs
+++ CurilClever2/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace CurilClever2.Models
   public class User
   {
     public int id { get; set; }
+    [MinLength(1, ErrorMessage = "NameMinLength")]
+    [Display(Name = "Name")]
+    [RegularExpression(@"^[a-zA-Zа-яА-Я ]{1,}$", ErrorMessage = "NameWrongName")]
     public string name { get; set; }
     public string Login { get; set; }
     public string PasswordHash { get; set; }
--- CurilClever2/ViewModels/ManageAccountViewModel.cs
+++ CurilClever2/ViewModels/ManageAccountViewModel.cs
@@ -9,25 +9,27 @@ namespace CurilClever2.ViewModels
 {
   public class ManageAccountViewModel
   {
-    [Display(Name = "Имя")]
-    [Required(ErrorMessage = "Не указан Имя")]
+    [Display(Name = "Name")]
+    [Required(ErrorMessage = "NameRequired")]
+    [MinLength(2, ErrorMessage = "NameMinLength")]
+    [RegularExpression(@"^[a-zA-Zа-яА-Я ]{1,}$", ErrorMessage = "NameWrongName")]
     public string Name { get; set; }
 
-    [Display(Name = "EMail")]
-    [Required(ErrorMessage = "Не указан Email")]
-    [DataType(DataType.EmailAddress, ErrorMessage = "Введите корректный email")]
+    [Display(Name = "Login")]
+    [Required(ErrorMessage = "LoginRequired")]
+    [DataType(DataType.EmailAddress, ErrorMessage = "LoginIncorrect")]
     public string Login { get; set; }
 
-    [Display(Name = "новый пароль")]
+    [Display(Name = "Password")]
     [DataType(DataType.Password)]
     public string Password { get; set; }
 
-    [Display(Name = "повторите пароль")]
+    [Display(Name = "ConfirmPassword")]
     [DataType(DataType.Password)]
-    [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+    [Compare("Password", ErrorMessage = "ConfirmPasswordCompare")]
     public string ConfirmPassword { get; set; }
 
-    [Display(Name = "Подписка на новости и обновления")]
+    [Display(Name = "EMailNewsSubscribe")]
     public bool EMailNewsSubscribe { get; set; }
 
     public ManageAccountViewModel()
